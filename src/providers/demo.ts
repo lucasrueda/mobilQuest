04b8230e68c054e8b3ebec2192ce5241dcd2e3b9
Ricky Bruno <ricardobruno_89@hotmail.com>
2019-01-29 10:40:50 -0300

    nada...

--- src/providers/demo.ts
+++ src/providers/demo.ts
@@ -4,7 +4,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DemoProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(
+    public http: HttpClient
+  ) {
+    
   }
 
   checkDemo() {
